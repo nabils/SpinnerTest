3ad87e744280b86905d2d4d2e10e5cf301a238a5
Nabil Shuhaiber <nabil@shuhaiber.com>
2015-05-23 00:51:33 +0100

    Initial Commit

---
+++ SpinnerTest/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace WpfApplication3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (this.Spinner.Visibility == Visibility.Collapsed || this.Spinner.Visibility == Visibility.Hidden)
+            {
+                this.Spinner.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                this.Spinner.Visibility = Visibility.Collapsed;
+            }
+        }
+    }
+}
---
+++ SpinnerTest/Spinner.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApplication3
+{
+    public class Spinner : Control
+    {
+        static Spinner()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(Spinner), new FrameworkPropertyMetadata(typeof(Spinner)));
+        }
+
+        public double Scale
+        {
+            get { return (double)GetValue(ScaleProperty); }
+            set { SetValue(ScaleProperty, value); }
+        }
+
+        public static readonly DependencyProperty ScaleProperty =
+            DependencyProperty.Register("Scale", typeof(double), typeof(Spinner), new PropertyMetadata(1.0));
+
+        
+    }
+}
